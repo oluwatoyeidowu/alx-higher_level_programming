6a2dc06ada70080dee558b488f2a95be56568dcf
oluwatoyeidowu <oagbonjinmi@gmail.com>
2022-09-19 21:04:51 -0700

    Shell script that runs a Python script

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,17 @@
+nclude <stdlib.h>
+
+#include "lists.h"
+
+
+
+/**
+ *
+ *  * check_cycle - Checks if a singly-linked list contains a cycle.
+ * @list: A singly-linked list.
+ *
+ * Return: If there is no cycle - 0.
+ * If there is a cycle - 1.
+ *
+ **/
+int check_cycle(listint_t *list)
+{
