0b8a78dde33fc1b8eff66900f9cd3f3d82a3fb3e
oluwatoyeidowu <oagbonjinmi@gmail.com>
2022-11-29 19:41:43 -0800

    SQL_introduction

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,6 @@
--- Lists all records of the table second_table.
--- Records are ordered by descending score.
-SELECT `score`, `name`
-FROM `second_table`
-ORDER BY `score` DESC;
+-- Creates and fills a table second_table with attributes id, name and score.
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
